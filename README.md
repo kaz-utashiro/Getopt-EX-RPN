5f8abb7547557849a0133f2fcc1fa039320c7e14
Kazumasa Utashiro <kaz@utashiro.com>
2022-09-28 14:00:04 +0900

    minil new Getopt::EX::RPN

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Getopt::EX::RPN - It's new $module
+
+# SYNOPSIS
+
+    use Getopt::EX::RPN;
+
+# DESCRIPTION
+
+Getopt::EX::RPN is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/Getopt/EX/RPN.pm
@@ -0,0 +1,37 @@
+package Getopt::EX::RPN;
+use 5.014;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Getopt::EX::RPN - It's new $module
+
+=head1 SYNOPSIS
+
+    use Getopt::EX::RPN;
+
+=head1 DESCRIPTION
+
+Getopt::EX::RPN is ...
+
+=head1 AUTHOR
+
+Kazumasa Utashiro
+
+=head1 LICENSE
+
+Copyright Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
